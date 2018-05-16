1d54e09806748ae112d37e5b3d8a703f62cb3273
jinyu73 <1115741577@qq.com>
2018-05-16 21:42:11 +0800

    element

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -19,7 +19,6 @@
                          <el-button type="warning">挂单</el-button>
                          <el-button type="danger">删除</el-button>
                          <el-button type="success">结账</el-button>
-                      
                       </div>
                   </el-tab-pane>
                   <el-tab-pane label="挂单">挂单</el-tab-pane>
@@ -38,16 +37,15 @@
                     </ul>
                   </div>
               </div>
-
               <div class="goods-type">
                 <el-tabs>
                   <el-tab-pane label="汉堡">
                     <div>
                       <ul class="cookList">
-                        <li>
-                          <span class="foodImg"><img /></span>
-                          <span class="foodName">香辣鸡腿堡</span>
-                          <span class="foodPrice">￥18元</span>
+                        <li v-for="goods in type0Goods">
+                          <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                          <span class="foodName">{{goods.goodsName}}</span>
+                          <span class="foodPrice">￥{{goods.price}}元</span>
                         </li>
                       </ul>
                     </div>
@@ -87,7 +85,7 @@ export default {
         count: 1
       }],
       oftenGoods: [{
-        goodsId:1,
+        goodsId: 1,
         goodsName: '香辣鸡腿堡',
         price: 18
       }, {
@@ -114,7 +112,60 @@ export default {
         goodsId: 7,
         goodsName: '美味薯条',
         price: 18
-      }]
+      }],
+	  type0Goods:[
+          {
+              goodsId:1,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+              goodsName:'香辣鸡腿堡',
+              price:18
+          }, {
+              goodsId:2,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'田园鸡腿堡',
+              price:15
+          }, {
+              goodsId:3,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+              goodsName:'和风汉堡',
+              price:15
+          }, {
+              goodsId:4,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'快乐全家桶',
+              price:80
+          }, {
+              goodsId:5,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'脆皮炸鸡腿',
+              price:10
+          }, {
+              goodsId:6,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+              goodsName:'魔法鸡块',
+              price:20
+          }, {
+              goodsId:7,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+              goodsName:'可乐大杯',
+              price:10
+          }, {
+              goodsId:8,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'雪顶咖啡',
+              price:18
+          }, {
+              goodsId:9,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'大块鸡米花',
+              price:15
+          }, {
+              goodsId:20,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'香脆鸡柳',
+              price:17
+          }  
+      ],
     }
   },
   mounted: function () {
@@ -149,4 +200,29 @@ export default {
                     color:#58b7ff
     .goods-type
       clear:both
+	.cookList 
+		li
+			list-style: none
+			width:23%
+			border:1px solid #e5e9f2
+			height:auto
+			overflow:hidden
+			background-color:#fff
+			padding:2px
+			float:left
+			margin:2px
+			span
+				display:block
+				float:left
+			.foodImg
+				width:40%
+			.foodName
+				font-size: 18px
+				padding-left:10px
+				color:brown
+			.foodPrice
+				font-size:16px
+				padding-left:10px
+				padding-top: 10px
+
 </style>
